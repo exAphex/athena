f39ecfe360430198cd81d091f1acf913c8925573
exAphex <aydin.tekin.66@hotmail.de>
2024-07-01 16:38:59 +0200

    added first endpoint

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 
 app.use(express.static("public"));
 
+// risk evaluation endpoint
+app.post("/visit", (req, res) => {
+                res.send("test");
+})
+
 app.listen(3000, () => {
  console.log("Server running on port 3000");
 });
